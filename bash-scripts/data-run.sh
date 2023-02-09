bc273bfb58ce6a69f3593c89a398468f0a564dbe
Arthur Guillaumin <ahw795@frontend8.apocrita>
2023-02-09 08:34:04 +0000

    Add poetry lock, bash script for data processing on QMUL server, and add some config parameters in config.ini

---
+++ bash-scripts/data-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#$ -cwd     
+#$ -pe smp 1      # Request 1 core
+#$ -l h_rt=1:0:0  # Request 1 hour runtime
+#$ -l h_vmem=10G   # Request 1GB RAM
+
+cd ..
+source ~/.bashrc
+poetry run mlflow run --experiment-name data-global --env-manager local -P lat_min=-85 -P lat_max=85 -P long_min=-280 -P long_max=80 -P factor=4 -P chunk_size=1 -P CO2=1 -P global=0 -P ntimes=10 .
