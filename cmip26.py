e06425c0a500538eccf2f3622fce28ca921ea7e7
arthur <ahw795@qmul.ac.uk>
2023-02-10 17:10:59 +0000

    Cleaning of data processing

--- cmip26.py
+++ cmip26.py
@@ -24,28 +24,29 @@ from data.pangeo_catalog import get_patch
 import logging
 import tempfile
 
-
-
-# logging config
+# load environment variables
+debug_mode = os.environ.get('DEBUG_MODE')
 logging_level = os.environ.get('LOGGING_LEVEL')
+
+# read config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# set logging config depending on the logging config
 if logging_level is not None:
     logging_level = getattr(logging, logging_level)
     logging.basicConfig(level=logging_level)
 logger = logging.getLogger(__name__)
-config = configparser.ConfigParser()
-config.read('config.ini')
+
 
 # Script parameters
-CATALOG_URL = 'https://raw.githubusercontent.com/pangeo-data/pangeo-datastore\
-/master/intake-catalogs/master.yaml'
+CATALOG_URL = 'https://raw.githubusercontent.com/pangeo-data/pangeo-datastore/master/intake-catalogs/master.yaml'
 
+
+# Parse the command-line parameters
 DESCRIPTION = 'Read data from the CM2.6 and \
         apply coarse graining. Stores the resulting dataset into an MLFLOW \
         experiment within a specific run.'
-
-data_location = tempfile.mkdtemp(dir=config['MLFLOW']['TEMP_DATA_LOCATION'])
-
-# Parse the command-line parameters
 parser = argparse.ArgumentParser(description=DESCRIPTION)
 parser.add_argument('bounds', type=float, nargs=4, help='min lat, max_lat,\
                     min_long, max_long')
@@ -64,14 +65,13 @@ params = parser.parse_args()
 
 
 # Retrieve the patch of data specified in the command-line args
-patch_data, grid_data = get_patch(CATALOG_URL, params.ntimes, params.bounds,
-                                  params.CO2, 'usurf', 'vsurf')
-
+patch_data, grid_data = get_patch(CATALOG_URL, params.ntimes, params.bounds, params.CO2, 'usurf', 'vsurf')
 logger.debug(patch_data)
 logger.debug(grid_data)
 
 # If global data, we make the dataset cyclic along longitude
 if params.global_ == 1:
+    # TODO this breaks currently. Not urgent though.
     logger.info('Cyclic data... Making the dataset cyclic along longitude...')
     patch_data = cyclize_dataset(patch_data, 'xu_ocean', params.factor)
     grid_data = cyclize_dataset(grid_data, 'xu_ocean', params.factor)
@@ -79,17 +79,18 @@ if params.global_ == 1:
     patch_data = patch_data.chunk(dict(xu_ocean=-1))
     grid_data = grid_data.chunk(dict(xu_ocean=-1))
 
-logger.debug('Getting grid data locally')
 # grid data is saved locally, no need for dask
+logger.debug('Getting grid data locally')
 grid_data = grid_data.compute()
 
-logger.debug('Mapping blocks')
 # Calculate eddy-forcing dataset for that particular patch
-debug_mode = os.environ.get('DEBUG_MODE')
-if params.factor != 0 and not debug_mode:
+logger.debug('Mapping blocks')
+
+if not debug_mode:
     scale_m = params.factor
     def func(block):
         return eddy_forcing(block, grid_data, scale=scale_m)
+
     template = patch_data.coarsen(dict(xu_ocean=int(scale_m),
                                        yu_ocean=int(scale_m)),
                                   boundary='trim').mean()
@@ -99,9 +100,6 @@ if params.factor != 0 and not debug_mode:
     #forcing = xr.map_blocks(func, patch_data, template=template)
     forcing = eddy_forcing(patch_data, grid_data, scale=scale_m, method='mean',
                             scale_mode='factor')
-elif not debug_mode:
-    scale_m = params.scale * 1e3
-    forcing = eddy_forcing(patch_data, grid_data, scale=scale_m, method='mean')
 else:
     logger.info('!!!Debug mode!!!')
     forcing = patch_data
@@ -117,26 +115,19 @@ if not debug_mode:
     forcing['usurf'].attrs['type'] = 'input'
     forcing['vsurf'].attrs['type'] = 'input'
 
-# Crop according to bounds
+# Crop according to bounds passed as CLI options
 bounds = params.bounds
-forcing = forcing.sel(xu_ocean=slice(bounds[2], bounds[3]),
-                      yu_ocean=slice(bounds[0], bounds[1]))
-
-# chunk_sizes = list(map(int, params.chunk_size.split('/')))
-# while len(chunk_sizes) < 3:
-#     chunk_sizes.append('auto')
-# forcing = forcing.chunk(dict(zip(('time', 'xu_ocean', 'yu_ocean'),
-#                                  chunk_sizes)))
-
-forcing = forcing.chunk(dict(time=1))
+forcing = forcing.sel(xu_ocean=slice(bounds[2], bounds[3]), yu_ocean=slice(bounds[0], bounds[1]))
 
 logger.info('Preparing forcing data')
 logger.debug(forcing)
 print(forcing)
+
 # export data
+data_location = tempfile.mkdtemp(dir=config['MLFLOW']['TEMP_DATA_LOCATION'])
 forcing.to_zarr(join(data_location, 'forcing'), mode='w')
 
-# Log as an artifact the forcing data
+# Log as an artifact the created zarr
 logger.info('Logging processed dataset as an artifact...')
 mlflow.log_artifact(join(data_location, 'forcing'))
 logger.info('Completed...')
