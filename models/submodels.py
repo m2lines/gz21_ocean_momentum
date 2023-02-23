52ba8b77d7a8efedd66975d701dfa33b09f42aee
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-02-23 14:11:36 +0000

    Add models/ files with docstrings and linting

--- models/submodels.py
+++ models/submodels.py
@@ -6,25 +6,28 @@ Created on Wed Nov  4 13:34:58 2020
 @author: arthur
 """
 
-from data.xrtransforms import (ScalingTransform, SeasonalStdizer,
-                               ChainedTransform, TargetedTransform,
-                               BZFormulaTransform)
+from data.xrtransforms import (
+    ScalingTransform,
+    SeasonalStdizer,
+    ChainedTransform,
+    TargetedTransform,
+    BZFormulaTransform,
+)
 
-velocity_vars = ['usurf', 'vsurf']
-forcing_vars = ['S_x', 'S_y']
+velocity_vars = ["usurf", "vsurf"]
+forcing_vars = ["S_x", "S_y"]
 
-velocity_scaler = TargetedTransform(ScalingTransform(10.), velocity_vars)
-forcing_scaler = TargetedTransform(ScalingTransform(1e7, inverse=False),
-                                   forcing_vars)
+velocity_scaler = TargetedTransform(ScalingTransform(10.0), velocity_vars)
+forcing_scaler = TargetedTransform(ScalingTransform(1e7, inverse=False), forcing_vars)
 
 monthly_stdizer_means = SeasonalStdizer(std=False)
 monthly_stdizer_means_stds = SeasonalStdizer(std=True)
 
-transform1 = ChainedTransform((velocity_scaler, forcing_scaler,
-                               monthly_stdizer_means))
+transform1 = ChainedTransform((velocity_scaler, forcing_scaler, monthly_stdizer_means))
 
-transform2 = ChainedTransform((velocity_scaler, forcing_scaler,
-                               monthly_stdizer_means_stds))
+transform2 = ChainedTransform(
+    (velocity_scaler, forcing_scaler, monthly_stdizer_means_stds)
+)
 
 transform3 = ChainedTransform((velocity_scaler, forcing_scaler))
 
