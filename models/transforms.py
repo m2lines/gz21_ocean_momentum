a2d9de11afc7934ad4cc04b6d06237684a8ffded
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-02-23 14:11:36 +0000

    Add models/ files with docstrings and linting

--- models/transforms.py
+++ models/transforms.py
@@ -61,8 +61,9 @@ class PrecisionTransform(Transform):
         # Careful: the split argument is the size of the sections, not the
         # number of them (although does not matter for 4 channels)
         result = torch.clone(input_)
-        result[:, self.indices, :, :] = self.transform_precision(
-            input_[:, self.indices, :, :]) + self.min_value
+        result[:, self.indices, :, :] = (
+            self.transform_precision(input_[:, self.indices, :, :]) + self.min_value
+        )
         return result
 
     @staticmethod
@@ -84,8 +85,11 @@ class MixedPrecisionTransform(PrecisionTransform):
     def transform(self, input_):
         result = super().transform(input_)
         result = torch.clone(result)
-        result[:, self.indices, :, :] = (1 / ((torch.abs(result[:, self.mean_indices, :, :]) + 0.01)) *
-                                         result[:, self.indices, :, :])
+        result[:, self.indices, :, :] = (
+            1
+            / ((torch.abs(result[:, self.mean_indices, :, :]) + 0.01))
+            * result[:, self.indices, :, :]
+        )
         return result
 
 
@@ -98,7 +102,7 @@ class SoftPlusTransform(PrecisionTransform):
         return softplus(precision)
 
     def __repr__(self):
-        return ''.join(('SoftPlusTransform(', str(self.min_value), ')'))
+        return "".join(("SoftPlusTransform(", str(self.min_value), ")"))
 
 
 class MixedSoftPlusTransform(MixedPrecisionTransform):
@@ -110,7 +114,7 @@ class MixedSoftPlusTransform(MixedPrecisionTransform):
         return softplus(precision)
 
     def __repr__(self):
-        return ''.join(('MixedSoftPlusTransform(', str(self.min_value), ')'))
+        return "".join(("MixedSoftPlusTransform(", str(self.min_value), ")"))
 
 
 class SquareTransform(PrecisionTransform):
@@ -122,7 +126,7 @@ class SquareTransform(PrecisionTransform):
         return precision**2
 
     def __repr__(self):
-        return ''.join(('SquareTransform(', str(self.min_value), ')'))
+        return "".join(("SquareTransform(", str(self.min_value), ")"))
 
 
 class MeanTransform(Transform):
@@ -146,7 +150,7 @@ class ComposeTransform(Transform):
         return input
 
     def __repr__(self):
-        return ' o '.join([t.__repr__() for t in self.transforms])
+        return " o ".join([t.__repr__() for t in self.transforms])
 
 
 class MeanPrecisionTransform(ComposeTransform):
