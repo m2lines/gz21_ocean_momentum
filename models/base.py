1b49111366d076c8f4e591045565c1f354f89bc5
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-02-27 17:53:46 +0000

    Linting and docstrings in models/

--- models/base.py
+++ models/base.py
@@ -1,36 +1,85 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun  3 17:18:53 2020
+Defines base classes with methods to be used in pytorch models of models1.py.
 
-@author: arthur
+TODO
+----
+- Should these really be defined here, or should they perhaps be defined directly in
+  FullyCNN of models1.py?  # AB
+- FinalTransformationMixin is not used anywhere as far as I can tell, should we remove?
+- output_width and output_height could probably be combined for one output_size method.
+- As far as I can tell the methods of DetectOutputSizeMixin are only used in
+  testing/utils.py and data/datasets.py so may be better off there than as part of the
+  model?
 """
 import torch
 
 
 class DetectOutputSizeMixin:
+    """Class to detect the shape of a neural net."""
+
     def output_width(self, input_height, input_width):
-        x = torch.zeros((1, self.n_in_channels, input_height, input_width))
-        x = x.to(device=self.device)
-        y = self(x)
-        # temporary fix for student loss
-        if isinstance(y, tuple):
-            y = y[0]
-        return y.size(3)
+        """
+        Generate a tensor and run forward model to get output width.
+
+        Parameters
+        ----------
+        input_height, output_height : int
+            The dimensions of the model input tensor
+
+        Returns
+        -------
+        dummy_out.size(3) : type?  # AB
+            width of the output tensor
+        """
+        dummy_in = torch.zeros((1, self.n_in_channels, input_height, input_width))
+        dummy_in = dummy_in.to(device=self.device)
+        # AB - Self here is assuming access to a neural net forward method?
+        #      If so I think this should really be contained in FullyCNN.
+        #      We can discuss and I am happy to perform the refactor.
+        dummy_out = self(dummy_in)
+
+        # temporary fix for student loss  # AB Is this still a temp. fix?
+        if isinstance(dummy_out, tuple):
+            dummy_out = dummy_out[0]
+        return dummy_out.size(3)
 
     def output_height(self, input_height, input_width):
-        x = torch.zeros((1, self.n_in_channels, input_height, input_width))
-        x = x.to(device=self.device)
-        y = self(x)
-        # temporary fix for student loss
-        if isinstance(y, tuple):
-            y = y[0]
-        return y.size(2)
+        """
+        Generate a tensor and run forward model to get output height.
+
+        Parameters
+        ----------
+        input_height, output_height : int
+            The dimensions of the model input tensor
+
+        Returns
+        -------
+        dummy_out.size(2) : type?  # AB
+            height of the output tensor
+        """
+        dummy_in = torch.zeros((1, self.n_in_channels, input_height, input_width))
+        dummy_in = dummy_in.to(device=self.device)
+        dummy_out = self(dummy_in)
+
+        # temporary fix for student loss  # AB Is this still a temp. fix?
+        if isinstance(dummy_out, tuple):
+            dummy_out = dummy_out[0]
+        return dummy_out.size(2)
 
     @property
     def device(self):
-        p = list(self.parameters())[0]
-        return p.device
+        """
+        Return the device model uses.
+
+        Returns
+        -------
+        type?  # AB
+            description?  # AB
+        """
+        params = list(self.parameters())[0]
+        return params.device
 
 
 class FinalTransformationMixin:
