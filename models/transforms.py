9f0e9d81ea96cb42a39ec6a4d48c6f41957d6c34
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-02-27 17:53:46 +0000

    Linting and docstrings in models/

--- models/transforms.py
+++ models/transforms.py
@@ -1,55 +1,96 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed May  6 19:38:09 2020
+Defines transformations applied to model outputs.
 
-@author: arthur
-In this file we define some transformations applied to the output of our 
-models. This allows us to keep separate these from the models themselves.
+Define some transformations applied to model outputs.
+Allows us to separate these operations from the models themselves.
 In particular, when we use a heteroskedastic loss, we compare two
 transformations that ensure that the precision is positive.
+
+TODO
+----
+- Not immediately clear that ABC (abstract base class) is useful here?
+- Seem to define lots of child classes to work to a fial point, can this be reduced?
 """
 
 from abc import ABC, abstractmethod
-from torch.nn import Module, Parameter
 import torch
+from torch import nn
 from torch.nn.functional import softplus
 
 
-class Transform(Module, ABC):
-    """Abstract Base Class for all transforms"""
+class Transform(nn.Module, ABC):
+    """
+    Base Class for all transforms, child of a Torch NN Module, does nothing.
 
-    def __init__(self):
-        super().__init__()
+    Methods
+    -------
+    transform
+    forward
+    """
 
+    # AB This does nothing, should it be defined in later classes directly?
     @abstractmethod
     def transform(self, input):
+        """Does nothing."""
         pass
 
     def forward(self, input_):
+        # AB Do we need this method if it only applies the transform method?
+        """
+        Apply self.transform to input.
+
+        Parameters
+        ----------
+        input_ : type?  # AB
+            description  # AB
+        """
         return self.transform(input_)
 
+    # AB This does nothing, should it be defined in later classes directly?
     @abstractmethod
     def __repr__(self):
+        """Does nothing."""
         pass
 
 
 class PrecisionTransform(Transform):
+    """
+    Class description?  # AB
+
+    Attributes
+    ----------
+    min_value : float
+      description?  # AB
+
+    Methods
+    -------
+    min_value : type?  # AB
+        description?  # AB
+    indices : type?  # AB
+        description?  # AB
+    transform
+    transform_precision
+    """
+
     def __init__(self, min_value=0.1):
         super().__init__()
-        self.min_value = Parameter(torch.tensor(min_value))
+        self.min_value = nn.Parameter(torch.tensor(min_value))
 
     @property
     def min_value(self):
+        """Applies softplus activation function to min_value."""
         return softplus(self._min_value)
 
     @min_value.setter
     def min_value(self, value):
-        self._min_value = Parameter(torch.tensor(value))
+        """Convert float input min_value into a Torch tensor."""
+        self._min_value = nn.Parameter(torch.tensor(value))
 
     @property
     def indices(self):
-        """Return the indices transformed"""
+        """Return the indices transformed."""
         return self._indices
 
     @indices.setter
@@ -57,9 +98,24 @@ class PrecisionTransform(Transform):
         self._indices = values
 
     def transform(self, input_):
-        # Split in sections of size 2 along channel dimension
-        # Careful: the split argument is the size of the sections, not the
-        # number of them (although does not matter for 4 channels)
+        # AB is this docstring correct? It looks to me like it
+        """
+        Split into sections of size 2 along channel dimension.
+
+        What is this method for?, longer description of purpose?  # AB
+        Note: the split argument is the size of the sections, not the number of them,
+        although this does not matter for 4 channels.
+
+        Parameters
+        ----------
+        input_ : type?  # AB
+            description?  # AB
+
+        Returns
+        -------
+        result : type?  # AB
+            description?  # AB
+        """
         result = torch.clone(input_)
         result[:, self.indices, :, :] = (
             self.transform_precision(input_[:, self.indices, :, :]) + self.min_value
@@ -69,20 +125,51 @@ class PrecisionTransform(Transform):
     @staticmethod
     @abstractmethod
     def transform_precision(precision):
+        """Does nothing."""
         pass
 
 
 class MixedPrecisionTransform(PrecisionTransform):
+    """
+    Class description?  # AB
+
+    Methods
+    -------
+    mean_indices
+    transform
+    """
+
     def __init__(self):
         super().__init__()
 
     @property
     def mean_indices(self):
-        s = set(self.indices)
-        s2 = set(list(range(4)))
-        return list(s2.difference(s))
+        """
+        description?  # AB
+
+        Returns
+        -------
+        : list
+            description  # AB
+        """
+        indices_set = set(self.indices)
+        range_set = set(list(range(4)))
+        return list(range_set.difference(indices_set))
 
     def transform(self, input_):
+        """
+        Description?  # AB
+
+        Parameters
+        ----------
+        input_ : type?  # AB
+            description?  # AB
+
+        Returns
+        -------
+        result : type?  # AB
+            description?  # AB
+        """
         result = super().transform(input_)
         result = torch.clone(result)
         result[:, self.indices, :, :] = (
@@ -94,6 +181,21 @@ class MixedPrecisionTransform(PrecisionTransform):
 
 
 class SoftPlusTransform(PrecisionTransform):
+    """
+    Class description?  # AB
+
+    Attributes
+    ----------
+    min_value : float
+      description?  # AB
+
+    Methods
+    -------
+    mean_indices
+    transform_precision
+    """
+
+    # AB This repeats the __init__ for PrecisionTransform so is unneccessary?
     def __init__(self, min_value=0.1):
         super().__init__(min_value)
 
@@ -106,8 +208,18 @@ class SoftPlusTransform(PrecisionTransform):
 
 
 class MixedSoftPlusTransform(MixedPrecisionTransform):
-    def __init__(self):
-        super().__init__()
+    """
+    Class description?  # AB
+
+    Attributes
+    ----------
+    min_value : float
+      description?  # AB
+
+    Methods
+    -------
+    transform_precision
+    """
 
     @staticmethod
     def transform_precision(precision):
@@ -118,6 +230,20 @@ class MixedSoftPlusTransform(MixedPrecisionTransform):
 
 
 class SquareTransform(PrecisionTransform):
+    """
+    Class description?  # AB
+
+    Attributes
+    ----------
+    min_value : float
+      description?  # AB
+
+    Methods
+    -------
+    transform_precision
+    """
+
+    # AB This repeats the __init__ for PrecisionTransform so is unneccessary?
     def __init__(self, min_value=0.1):
         super().__init__(min_value)
 
@@ -130,6 +256,14 @@ class SquareTransform(PrecisionTransform):
 
 
 class MeanTransform(Transform):
+    """
+    Class description?  # AB
+
+    Methods
+    -------
+    transform
+    """
+
     def transform(self, input):
         result = torch.clone(input)
         result[:, 0:2, :, :] = torch.tan(result[:, 0:2, :, :])
@@ -140,22 +274,32 @@ class MeanTransform(Transform):
 
 
 class ComposeTransform(Transform):
+    """
+    Class description?  # AB
+
+    Methods
+    -------
+    transform
+    """
+
     def __init__(self, transforms):
         super().__init__()
         self.transforms = transforms
 
     def transform(self, input):
-        for t in self.transforms:
-            input = t(input)
+        for transform in self.transforms:
+            input = transform(input)
         return input
 
     def __repr__(self):
-        return " o ".join([t.__repr__() for t in self.transforms])
+        return " o ".join([transform.__repr__() for transform in self.transforms])
 
 
 class MeanPrecisionTransform(ComposeTransform):
+    """Class description?"""  # AB
+
     def __init__(self):
-        t1 = SoftPlusTransform()
-        t1.indices = [2, 3]
-        t2 = MeanTransform()
-        super().__init__((t1, t2))
+        transform_1 = SoftPlusTransform()
+        transform_1.indices = [2, 3]
+        transform_2 = MeanTransform()
+        super().__init__((transform_1, transform_2))
