e0e20800cc463ed746b7397460d45dcbd0e84f5d
Ben Orchard <thefirstmuffinman@gmail.com>
2023-09-01 15:23:53 +0100

    tweak MLproject, readme, data step CLI help

--- src/gz21_ocean_momentum/cli/data.py
+++ src/gz21_ocean_momentum/cli/data.py
@@ -4,10 +4,13 @@ from   gz21_ocean_momentum.common.bounding_box import BoundingBox
 
 import configargparse
 
-DESCRIPTION = "GZ21 data step: download CM2.6 dataset, apply coarse graining\
+# up to date as of 2023-09-01
+DEF_CATALOG_URI = "https://raw.githubusercontent.com/pangeo-data/pangeo-datastore/d684158e92fb3f3ad3b34e7dc5bba52b22a3ba80/intake-catalogs/ocean.yaml"
+
+DESCRIPTION = "GZ21 data step: download CM2.6 dataset, apply coarse graining \
 and generate forcings. Saves result to disk in zarr format."
 
-p = configargparse.ArgParser()
+p = configargparse.ArgParser(description=DESCRIPTION)
 p.add("--config-file", is_config_file=True, help="config file path")
 p.add("--out-dir",  type=str,   required=True, help="folder to save generated forcings to (in zarr format)" )
 p.add("--lat-min",  type=float, required=True, help="bounding box minimum latitude")
@@ -15,10 +18,10 @@ p.add("--lat-max",  type=float, required=True, help="bounding box maximum latitu
 p.add("--long-min", type=float, required=True, help="bounding box minimum longitude")
 p.add("--long-max", type=float, required=True, help="bounding box maximum longitude")
 p.add("--cyclize",  action="store_true", help="global data; make cyclic along longitude")
-p.add("--ntimes",   type=int,   help="number of time points to process, starting from the first. Note that the CM2.6 dataset is daily, so this would be number of days.")
+p.add("--ntimes",   type=int,   help="number of time points to process, starting from the first. Note that the CM2.6 dataset is daily, so this would be number of days. If unset, uses whole dataset.")
 p.add("--co2-increase", action="store_true", help="use 1%% annual CO2 increase CM2.6 dataset. By default, uses control (no increase)")
 p.add("--factor",   type=int,   required=True, help="resolution degradation factor")
-p.add("--pangeo-catalog-uri", type=str, default="https://raw.githubusercontent.com/pangeo-data/pangeo-datastore/master/intake-catalogs/ocean.yaml", help="URI to Pangeo ocean dataset intake catalog file")
+p.add("--pangeo-catalog-uri", type=str, default=DEF_CATALOG_URI, help="URI to Pangeo ocean dataset intake catalog file")
 
 options = p.parse_args()
 
